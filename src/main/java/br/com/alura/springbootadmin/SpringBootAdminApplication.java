b4946e27a397a5a411cfdf3c156e4979ef30aa3b
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-12 19:48:32 -0300

    Configure authentication route actuator spring boot admin

--- src/main/java/br/com/alura/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/br/com/alura/springbootadmin/SpringBootAdminApplication.java
@@ -1,13 +1,16 @@
 package br.com.alura.springbootadmin;
 
+import br.com.alura.springbootadmin.config.SecurityConfigurations;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@Import({SecurityConfigurations.class})
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
